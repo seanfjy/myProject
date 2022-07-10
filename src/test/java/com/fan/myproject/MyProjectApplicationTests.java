d5d393870df71d6c9c22f15c305bad564e9f1617
fjy <seanfjy@qq.com>
2022-07-10 22:19:22 +0800

    Initial commit

---
+++ src/test/java/com/fan/myproject/MyProjectApplicationTests.java
@@ -0,0 +1,15 @@
+package com.fan.myproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyProjectApplicationTests {
+
+    @Test
+    void contextLoads() {
+        System.out.println("hello");
+        System.out.println("你好");
+    }
+
+}
